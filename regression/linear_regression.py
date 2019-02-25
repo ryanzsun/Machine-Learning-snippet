7e8f8a921fb96f13dd087115e72fb307be38bec6
zhensun <ryansun1984@gmail.com>
2019-02-25 19:14:47 +0800

    lr

--- regression/linear_regression.py
+++ regression/linear_regression.py
@@ -1,1 +1,20 @@
 import numpy as np
+from sklearn.datasets import load_boston
+
+class LinearRegression():
+    def __init__(self, learning_rate, convergence_diff):
+        self.lr = learning_rate
+        self.eps = convergence_diff
+        pass
+
+    def fit(self, data, label):
+        pass
+
+    def predict(self, data):
+        pass
+
+    def stochastic_gradient_descent(self):
+        pass
+
+    def loss(self, prediction, target):
+        pass
